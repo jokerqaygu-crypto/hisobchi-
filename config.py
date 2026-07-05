02f6f3b859e9e7353bb75288b24fc320a389af92
jokerqaygu-crypto <jokerqaygu@gmail.com>
2026-07-06 04:45:43 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,3 @@
-# ============================
-# Bot sozlamalari
-# ============================
-import os
-
-BOT_TOKEN = os.getenv("7021374608:AAE-LbkzA_s9M2FrJw3v6szOXW4WGJrZKYg")
-
-# Admin(lar) Telegram ID raqami(lari)
+BOT_TOKEN = "7021374608:AAE-LbkzA_s9M2FrJw3v6szOXW4WGJrZKYg"
 ADMIN_IDS = [5826150796]
-
 DB_NAME = "database.db"
